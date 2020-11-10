dc97a9a8c10341530869db451370bf3af0d9d9e4
Emma Sarfati <emmasarfati@MacBook-Pro-de-Emma.local>
2020-11-10 11:11:15 +0100

    prof reco + flake

--- pandas_questions.py
+++ pandas_questions.py
@@ -80,8 +80,8 @@ def plot_referendum_map(referendum_result_by_regions):
     mapreg = gpd.read_file('data/regions.geojson')
     referendum_result_by_regions = referendum_result_by_regions.join(
         mapreg.set_index('code'))
-    referendum_result_by_regions['ratio'] = 
-    referendum_result_by_regions['Choice A'] / (
+    referendum_result_by_regions['ratio'] = referendum_result_by_regions[
+        'Choice A'] / (
         referendum_result_by_regions['Choice A'] + 
         referendum_result_by_regions['Choice B'])
     gpd.GeoDataFrame(referendum_result_by_regions, 
