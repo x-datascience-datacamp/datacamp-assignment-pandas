2525b69751e587388e5aec57e00a9d4ce483172a
LoicJOURDAN <loic.jourdan@ensae-paristech.fr>
2020-11-10 16:25:59 +0100

    Before_flake

--- pandas_questions.py
+++ pandas_questions.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-    referendum = pd.DataFrame({})
-    regions = pd.DataFrame({})
-    departments = pd.DataFrame({})
+
+    referendum = pd.read_csv('data/referendum.csv', sep=';')
+    regions = pd.read_csv('data/regions.csv')
+    departments = pd.read_csv('data/departments.csv')
 
     return referendum, regions, departments
 
@@ -28,8 +29,10 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-    fkz=0
-    return pd.DataFrame({})
+    df = regions.merge(departments, left_on='code',
+                       right_on='region_code',
+                       suffixes=('_reg', '_dep'))
+    return df[['code_reg', 'name_reg', 'code_dep', 'name_dep']]
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
@@ -38,8 +41,25 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
-    return pd.DataFrame({})
+    if referendum.dropna().shape != referendum.shape:
+        raise ValueError("Missing values")
+    referendum = referendum.dropna()
+    index = {'1': '01', '2': '02', '3': '03', '4': '04', '5': '05',
+             '6': '06', '7': '07', '8': '08', '9': '09'}
+    for key, value in index.items():
+        referendum.loc[referendum['Department code'] == key,
+                       'Department code'] = value
+    list_dep = list(regions_and_departments.code_dep)
+    referendum[referendum['Department code'].isin(list_dep)]
+    regions_and_departments[regions_and_departments['code_dep'].isin(list_dep)]
+    df = referendum.merge(regions_and_departments, left_on='Department code',
+                          right_on='code_dep', suffixes=(False, False))
+    df = df[['Department code', 'Department name',
+             'Town code', 'Town name', 'Registered',
+             'Abstentions', 'Null', 'Choice A',
+             'Choice B', 'code_dep', 'code_reg',
+             'name_reg', 'name_dep']]
+    return df
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
@@ -48,8 +68,14 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
-    return pd.DataFrame({})
+    output = referendum_and_areas.groupby(["code_reg"]).agg({
+                       'name_reg': 'first',
+                       'Registered': sum,
+                       'Abstentions': sum,
+                       'Null': sum,
+                       'Choice A': sum,
+                       'Choice B': sum})
+    return output
 
 
 def plot_referendum_map(referendum_result_by_regions):
@@ -61,8 +87,18 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
-    return gpd.GeoDataFrame({})
+    reg_map = gpd.read_file('data/regions.geojson')
+    merged_map_df = referendum_result_by_regions.merge(reg_map,
+                                                       left_on='name_reg',
+                                                       right_on='nom',
+                                                       suffixes=(False, False))
+    col_A = merged_map_df['Choice A']
+    col_B = merged_map_df['Choice B']
+    merged_map_df['exprime'] = col_A + col_B
+    merged_map_df['ratio'] = col_A / merged_map_df['exprime']
+    df_final = merged_map_df[['ratio', 'name_reg', 'geometry']]
+    gdf = gpd.GeoDataFrame(df_final, geometry='geometry')
+    return gdf
 
 
 if __name__ == "__main__":
