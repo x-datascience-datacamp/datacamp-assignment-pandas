9cd95d22bfafe8c1b523fba94a1da323efb2cfba
Nadir Abdou <nadir.abdou@telecom-paristech.fr>
2020-11-11 11:07:03 +0100

    final result

--- pandas_questions.py
+++ pandas_questions.py
@@ -24,7 +24,7 @@ def load_data():
 
 def merge_regions_and_departments(regions, departments):
     """Merge regions and departments in one DataFrame.
-    
+
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
