98916efbbe491262f7def2c3fa9bd0c6fc2eff8b
unknown <taoufik.aghris@emines.um6p.ma>
2020-11-11 09:08:57 +0100

    Updated

--- pandas_questions.py
+++ pandas_questions.py
@@ -11,14 +11,16 @@ aggregate them by regions and finally plot them on a map using `geopandas`.
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+Dir = Path('.')
 
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-    referendum = pd.DataFrame({})
-    regions = pd.DataFrame({})
-    departments = pd.DataFrame({})
-
+    referendum = pd.read_csv(Dir / "data" / "referendum.csv", sep=";")
+    regions = pd.read_csv(Dir / "data" / "regions.csv")
+    departments = pd.read_csv(Dir / "data" / "departments.csv")
     return referendum, regions, departments
 
 
@@ -28,8 +30,14 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
-    return pd.DataFrame({})
+    reg_and_dep = pd.merge(
+            regions, departments,
+            how='inner',
+            left_on="code",
+            right_on="region_code",
+            suffixes=["_reg", "_dep"])
+    reg_and_dep = reg_and_dep[['code_reg', 'name_reg', 'code_dep', 'name_dep']]
+    return reg_and_dep
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
@@ -38,8 +46,18 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
-    return pd.DataFrame({})
+    # Pad strings in the Series/Index by prepending ‘0’ characters.1 -> 01
+    referendum['Department code'] = referendum['Department code'].str.zfill(2)
+    ref_reg_and_dep = pd.merge(
+            referendum, regions_and_departments,
+            left_on="Department code",
+            right_on="code_dep")
+    # No need to drop DOM-TOM-COM and the french living abroad
+    # because merge will remove them automatically with intersetion operation
+    ref_reg_and_dep = pd.merge(referendum, regions_and_departments,
+                               left_on="Department code",
+                               right_on="code_dep")
+    return ref_reg_and_dep
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
@@ -48,8 +66,15 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
-    return pd.DataFrame({})
+    columns = ["code_reg", "name_reg"]
+    regions_groups = referendum_and_areas.groupby(
+        columns, as_index=False
+    )
+    count_by_regions = regions_groups[
+        ["Registered", "Abstentions", "Null", "Choice A", "Choice B"]
+    ].sum()
+    # Setting index on code_reg
+    return count_by_regions.set_index('code_reg')
 
 
 def plot_referendum_map(referendum_result_by_regions):
@@ -61,8 +86,25 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
-    return gpd.GeoDataFrame({})
+    geo_df = gpd.read_file(Dir / "data" / "regions.geojson", sep=";")
+    # Merge these info into `referendum_result_by_regions` and dropping
+    # duplicate name column
+    regions_geometry = pd.merge(
+        referendum_result_by_regions,
+        geo_df,
+        left_on="code_reg",
+        right_on="code"
+    ).drop(['nom'], axis=1)
+    # compute ratio of choice A over all registered
+    regions_geometry['ratio'] = (
+        regions_geometry['Choice A'] /
+        (regions_geometry['Registered'] -
+         regions_geometry['Abstentions'] - regions_geometry['Null'])
+    )
+    # plotting and returning results
+    geo_regions_ratios = gpd.GeoDataFrame(regions_geometry)
+    geo_regions_ratios.plot(column="ratio")
+    return geo_regions_ratios
 
 
 if __name__ == "__main__":
