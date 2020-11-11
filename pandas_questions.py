ce82518eb057875cfcd27bc83ac877617692ebe0
Aristotelis Koen <Aris@Aristoteliss-MacBook-Pro-2.local>
2020-11-11 16:06:53 +0100

    results2

--- pandas_questions.py
+++ pandas_questions.py
@@ -1,4 +1,9 @@
-"""Plotting referendum results in pandas.
+
+"""Plotting referendum results in pandas."""
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+"""
 In short, we want to make beautiful map to report results of a referendum. In
 some way, we would like to depict results with something similar to the maps
 that you can find here:
@@ -6,9 +11,6 @@ https://github.com/x-datascience-datacamp/datacamp-assignment-pandas/blob/main/e
 To do that, you will load the data as pandas.DataFrame, merge the info and
 aggregate them by regions and finally plot them on a map using `geopandas` o.
 """
-import pandas as pd
-import geopandas as gpd
-import matplotlib.pyplot as plt
 
 
 def load_data():
@@ -24,7 +26,8 @@ def load_data():
 
 
 def merge_regions_and_departments(regions, departments):
-    """Merge regions and departments in one DataFrame.
+    """Merge regions and departments in one DataFrame."""
+    """
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
@@ -38,8 +41,8 @@ def merge_regions_and_departments(regions, departments):
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
-    """Merge referendum and regions_and_departments in one DataFrame.
-    You can drop the lines relative to DOM-TOM-COM departments, and the
+    """Merge referendum and regions_and_departments in one DataFrame."""
+    """You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
     columnorder = ['code_dep', 'code_reg', 'name_reg', 'name_dep']
@@ -62,8 +65,8 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
-    """Return a table with the absolute count for each region.
-    The return DataFrame should be indexed by `code_reg` and have columns:
+    """Return a table with the absolute count for each region."""
+    """The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
     columns = ['code_reg', 'name_reg', 'Registered', 'Abstentions',
@@ -75,8 +78,8 @@ def compute_referendum_result_by_regions(referendum_and_areas):
 
 
 def plot_referendum_map(referendum_result_by_regions):
-    """Plot a map with the results from the referendum.
-    * Load the geographic data with geopandas from `regions.geojson`.
+    """Plot a map with the results from the referendum."""
+    """* Load the geographic data with geopandas from `regions.geojson`.
     * Merge these info into `referendum_result_by_regions`.
     * Use the method `GeoDataFrame.plot` to display the result map. The results
       should display the rate of 'Choice A' over all expressed ballots.
