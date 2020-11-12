a6ff26421502c44a595a3c411f7811aca5df4f5e
unknown <arab.nawel@outlook.com>
2020-11-12 11:23:42 +0100

    Pandas Assignment second try

--- pandas_questions.py
+++ pandas_questions.py
@@ -1,4 +1,5 @@
 """Plotting referendum results in pandas.
+
 In short, we want to make beautiful map to report results of a referendum. In
 some way, we would like to depict results with something similar to the maps
 that you can find here:
@@ -22,15 +23,18 @@ def load_data():
 
 
 def merge_regions_and_departments(regions, departments):
-    """Merge regions and departments in one DataFrame.
+    """Merge regions and departments.
+
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
+
     Parameters
     ----------
     regions: pandas.DataFrame
-        contains information about regions
+        region's information
     departments: pandas.DataFrame
-        contains information about departments
+        departments information
+
     Returns
     -------
     df : pandas.DataFrame
@@ -49,83 +53,78 @@ def merge_regions_and_departments(regions, departments):
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
     """Merge referendum and regions_and_departments in one DataFrame.
+
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
+
     Parameters
     ----------
     referendum: pandas.DataFrame
-        contains information about referendums in different departments
+        information about referendums in different departments
     regions_and_departments: pandas.DataFrame
-        contains information about departments and their regions
+        information about departments and their regions
+
     Returns
     -------
     df : pandas.DataFrame
         merged dataframes
     """
-    # Removing DOM-TOM-COM and French living abroad
-    dep_codes = np.unique(referendum["Department code"])
-    dep_codes = [el for el in dep_codes if not el.isalpha()]
-    df = referendum.loc[referendum["Department code"].isin(dep_codes)].copy()
-
-    # Reverting department codes to standard format
-    # i.e: removing '0' if code is '01'
-    regions_and_departments['code_dep'] = regions_and_departments[
-        'code_dep'].apply(lambda x: x[1] if x[0] == '0' else x)
-
-    # Merging
-    df = pd.merge(df,
-                  regions_and_departments,
-                  left_on='Department code',
-                  right_on='code_dep',
-                  how='left'
-                  )
-
+    referendum = referendum[~referendum['Department code'].str.contains('Z')]
+    code_series = referendum["Department code"].str.pad(2, fillchar="0")
+    referendum.loc[:, "Department code"] = code_series
+    df = pd.merge(referendum, regions_and_departments,
+                                    how='left',
+                                    left_on='Department code',
+                                    right_on='code_dep')
+                                
     return df
+
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
     """Return a table with the absolute count for each region.
+
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
+
     Parameters
     ----------
     referendum_and_areas: pandas.DataFrame
         contains information about referendums in different departments
+
     Returns
     -------
     df : pandas.DataFrame
         dataframe grouped by region
     """
-    df = referendum_and_areas.groupby(["name_reg"]).sum()
-    df["name_reg"] = list(df.index)
-    df.index = list(range(df.shape[0]))
-    cols = ['name_reg',
-            'Registered',
-            'Abstentions',
-            'Null',
-            'Choice A',
-            'Choice B']
+    referendum_and_areas = referendum_and_areas.drop("Town code", axis=1)
+    referendum_results = referendum_and_areas.groupby(
+                            ["code_reg", "name_reg"]).sum()
+    referendum_results.reset_index().set_index("code_reg")
 
-    return df[cols]
+    return referendum_results.reset_index().set_index("code_reg")
 
 
 def plot_referendum_map(referendum_result_by_regions):
     """Plot a map with the results from the referendum.
+
     * Load the geographic data with geopandas from `regions.geojson`.
     * Merge these info into `referendum_result_by_regions`.
     * Use the method `GeoDataFrame.plot` to display the result map. The results
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
+
     Parameters
     ----------
     referendum_result_by_region: pandas.DataFrame
         contains summary about the referendum by region
+
     Returns
     -------
     geopandas.GeoDataFrame object
     """
-    region_map = gpd.read_file("./data/regions.geojson")
-    df = region_map.merge(referendum_result_by_regions,
+    geopd = gpd.read_file("./data/regions.geojson")
+    df = geopd.merge(referendum_result_by_regions,
                           left_on="nom",
                           right_on="name_reg",
                           how="right"
