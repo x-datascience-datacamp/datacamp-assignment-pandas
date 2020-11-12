662e5c67c96d501c800b485429b02be6d96c8524
cadmoska <cadmos.ka@gmail.com>
2020-11-12 11:30:47 +0100

    pull req3

--- pandas_questions.py
+++ pandas_questions.py
@@ -16,13 +16,15 @@ import matplotlib.pyplot as plt
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
     referendum = pd.read_csv(
-        'data/referendum.csv', header=0, sep=';',
-        error_bad_lines=False)
+        'data/referendum.csv', header=0, sep=';')
     regions = pd.read_csv(
-        'data/regions.csv', header=0, sep=',', error_bad_lines=False)
+        'data/regions.csv', header=0, sep=',')
     departments = pd.read_csv(
         'data/departments.csv', header=0,
-        sep=',', error_bad_lines=False)
+        sep=',')
+    referendum = referendum.dropna()
+    regions = regions.dropna()
+    departments = departments.dropna()
 
     return referendum, regions, departments
 
@@ -48,25 +50,42 @@ def merge_regions_and_departments(regions, departments):
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
     """Merge referendum and regions_and_departments in one DataFrame.
-
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-    regions_and_departments['name_dep'] = regions_and_departments[
-        'name_dep'].str.replace(
-            '-', ' ').str.replace(
-                'ô', 'o').str.replace(
-                    'é', 'e').str.replace('è', 'e').str.upper()
+
+    referendum = referendum.dropna()
+    regions_and_departments.dropna()
+    dom_tom_com = referendum[referendum[
+        'Department code'].str.startswith('Z')].index
+    referendum = referendum.drop(dom_tom_com)
+    # regions_and_departments['name_dep'] = regions_and_departments[
+    #     'name_dep'].str.replace(
+    #         '-', ' ').str.replace(
+    #             'ô', 'o').str.replace(
+    #                 'é', 'e').str.replace(
+    #                     'è', 'e').str.replace(
+    #                         'CORSE DU SUD', 'CORSE SUD').str.upper()
+    regions_and_departments['code_dep'] = regions_and_departments[
+        'code_dep'].str.replace(
+            '01', '1').str.replace(
+                '02', '2').str.replace(
+                    '03', '3').str.replace(
+                        '04', '4').str.replace(
+                            '05', '5').str.replace(
+                                '06', '6').str.replace(
+                                    '07', '7').str.replace(
+                                        '08', '8').str.replace(
+                                            '09', '9')
+    regions_and_departments.drop(columns='name_dep')
+
     merged_referendum_and_areas = pd.merge(
         regions_and_departments,
         referendum.rename(
             columns={
-                'Department code': 'code_dep',
-                'Department name': 'name_dep'}), on='name_dep', how='right')
-    dom_tom_com = merged_referendum_and_areas[merged_referendum_and_areas[
-        'code_dep_y'].str.startswith('Z')].index
-    merged_referendum_and_areas = merged_referendum_and_areas.drop(dom_tom_com)
-
+                'Department code': 'code_dep'}), on='code_dep', how='right')
+    merged_referendum_and_areas[
+        'Department code'] = merged_referendum_and_areas['code_dep']
     return merged_referendum_and_areas
 
 
@@ -102,7 +121,8 @@ def plot_referendum_map(referendum_result_by_regions):
     geographic = pd.DataFrame(gpd.read_file('data/regions.geojson'))
 
     ratio = referendum_result_by_regions['Choice A']/(
-        referendum_result_by_regions['Registered'])
+        referendum_result_by_regions[
+            'Choice A'] + referendum_result_by_regions['Choice B'])
     referendum_result_by_regions['ratio'] = ratio
 
     referendum_result_by_regions = pd.merge(
@@ -112,7 +132,6 @@ def plot_referendum_map(referendum_result_by_regions):
     referendum_result_by_regions = gpd.GeoDataFrame(
         referendum_result_by_regions)
     referendum_result_by_regions.plot(column='ratio')
-
     return referendum_result_by_regions
 
 
